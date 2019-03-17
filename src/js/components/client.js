2144b309663b8db8ca4c48ff3415bd1dcf9ff405
Andrew Downie <andrew_downie@manulife.com>
2019-03-17 15:25:20 -0400

    Ready for tut 5.

--- src/js/components/client.js
+++ src/js/components/client.js
@@ -1,4 +1,4 @@
-import {createStore} from 'redux';
+import {combineReducers, createStore} from 'redux';
 
 const reducer = function(state, action){
     if(action.type === "INC"){
@@ -10,15 +10,46 @@ const reducer = function(state, action){
     return state;
 }
 
-const store = createStore(reducer, 0);
+const userReducer = (state={}, action) => {
+
+    switch(action.type){
+        case "CHANGE_NAME": {
+            state = {...state, name: action.payload};
+            break;
+        }
+        case "CHANGE_AGE": {
+            state = {...state, age: age.payload};
+            break;
+        }
+    }
+
+    return state;
+}
+
+const tweetsReducer = (state={}, actions) => {
+
+}
+
+const reducer = combineReducers({
+    user: userReducer,
+    tweets: tweetsReducer,
+})
+
+const store = createStore(reducers, {
+    user: {
+        name: "will",
+        age: 35,
+    },
+    tweets: []
+});
 
 
 store.subscribe(() => {
     console.log("store changed", store.getState());
 })
 
-store.dispatch({type: "INC", payload: 1})
-store.dispatch({type: "INC", payload: 1})
+store.dispatch({type: "CHANGE_NAME", payload: "Will"})
+store.dispatch({type: "CHANGE_AGE", payload: 35})
 store.dispatch({type: "INC", payload: 1})
 store.dispatch({type: "INC", payload: 1})
 store.dispatch({type: "DEC", payload: 1})
